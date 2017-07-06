083b905517baca840b7d0d219685c459e794fc66
Mfundo07 <mfundombokoma07@gmail.com>
2017-07-06 16:11:27 +0200

    update of the play button and the pause mechanism

--- app/src/main/java/com/example/android/mediaplayer/SongsFragment.java
+++ app/src/main/java/com/example/android/mediaplayer/SongsFragment.java
@@ -1,37 +1,30 @@
 package com.example.android.mediaplayer;
 
-import android.content.ContentResolver;
-import android.content.ContentUris;
 import android.content.Context;
-import android.content.res.AssetFileDescriptor;
 import android.database.Cursor;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
-import static android.R.attr.id;
-import static android.media.CamcorderProfile.get;
-import static android.os.Build.VERSION_CODES.M;
+import static com.example.android.mediaplayer.R.id.play;
 
 /**
  * Created by Admin on 6/28/2017.
@@ -39,16 +32,26 @@ import static android.os.Build.VERSION_CODES.M;
 
 public class SongsFragment extends Fragment {
 
+    private static final int UPDATE_FREQUENCY = 500;
+    private static final int STEP_VALUE = 4000;
+
     private MediaPlayer sMediaPlayer;
     private AudioManager sAudioManager;
-    private ImageButton pauseButton;
     private ImageButton playButton;
-    private ImageButton fast_forward;
     private  ImageButton rewind;
-    boolean paused = true;
-    private int rewind_sec = 0;
-    private ImageButton stop;
-    final ArrayList<Song> songs = new ArrayList<Song>();
+    private TextView selectedFile = null;
+    private boolean isStarted = true;
+    private String currentFile = "";
+    private final Handler handler = new Handler();
+    private LinearLayout playView;
+
+
+    private final Runnable updatePositionRunnable = new Runnable() {
+        public void run() {
+            updatePosition();
+        }
+    };
+
 
     private AudioManager.OnAudioFocusChangeListener sOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener(){
         @Override
@@ -62,11 +65,28 @@ public class SongsFragment extends Fragment {
                 releaseMediaPlayer();
             }       }
     };
+
+
     private MediaPlayer.OnCompletionListener sOnCompletionListener = new MediaPlayer.OnCompletionListener(){
         @Override
         public void onCompletion(MediaPlayer mp) {
-           releaseMediaPlayer();
+            stopPlay();
     }};
+
+
+
+
+    private MediaPlayer.OnErrorListener onError = new MediaPlayer.OnErrorListener() {
+
+
+        @Override
+        public boolean onError(MediaPlayer mp, int what, int extra) {
+
+            return false;
+        }
+    };
+
+
 
 
     @Nullable
@@ -75,24 +95,32 @@ public class SongsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.song_list,container,false);
 
         sAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+        playButton = (ImageButton) rootView.findViewById(R.id.play);
+        ImageButton fast_forward = (ImageButton) rootView.findViewById(R.id.next);
+        rewind = (ImageButton) rootView.findViewById(R.id.prev);
+        selectedFile = (TextView) rootView.findViewById(R.id.selectedfile);
+        playButton = (ImageButton) rootView.findViewById(play);
+        selectedFile = (TextView) rootView.findViewById(R.id.selectedfile);
+        playView = (LinearLayout) rootView.findViewById(R.id.play_view);
+        playView.setVisibility(View.INVISIBLE);
+
+
 
         final ArrayList<Song> songs = new ArrayList<Song>();
-        songs.add(new Song("Mask Off (Remix)","Future ft Kendrick Lamar",R.raw.audio,R.drawable.ic_play_arrow_black));
-        songs.add(new Song("Colors of the wind", "Vannessa Williams",R.raw.colors_of_the_wind,R.drawable.ic_play_arrow_black));
-        songs.add(new Song("Don't dream it's over", "Crowded House",R.raw.dont_dream_its_over,R.drawable.ic_play_arrow_black));
-        songs.add(new Song("Let's get retarded ", "The Black Eyed Peas",R.raw.lets_get_retarded,R.drawable.ic_play_arrow_black));
-        songs.add(new Song("Hit me baby one more time", "Britney Spears",R.raw.hit_me_baby_one_more_time,R.drawable.ic_play_arrow_black));
-        songs.add(new Song("Niggas in Paris", "Jay-Z & Kanye West",R.raw.niggas_in_paris,R.drawable.ic_play_arrow_black));
-        songs.add(new Song("See you Again", "Miley Cyrus",R.raw.see_you_again,R.drawable.ic_play_arrow_black));
-        songs.add(new Song("Work it", "Missy Elliot",R.raw.missy_workit,R.drawable.ic_play_arrow_black));
-        songs.add(new Song("Bang Bang Bang", "Mark Ronson",R.raw.bang_bang_bang,R.drawable.ic_play_arrow_black));
-        songs.add(new Song("Unbreak my Heart", "Toni braxton",R.raw.unbreak_my_heart,R.drawable.ic_play_arrow_black));
+        songs.add(new Song("Mask Off (Remix)","Future ft Kendrick Lamar",R.raw.audio,android.R.drawable.ic_media_play));
+        songs.add(new Song("Colors of the wind", "Vannessa Williams",R.raw.colors_of_the_wind,android.R.drawable.ic_media_play));
+        songs.add(new Song("Don't dream it's over", "Crowded House",R.raw.dont_dream_its_over,android.R.drawable.ic_media_play));
+        songs.add(new Song("Let's get retarded ", "The Black Eyed Peas",R.raw.lets_get_retarded,android.R.drawable.ic_media_play));
+        songs.add(new Song("Hit me baby one more time", "Britney Spears",R.raw.hit_me_baby_one_more_time,android.R.drawable.ic_media_play));
+        songs.add(new Song("Niggas in Paris", "Jay-Z & Kanye West",R.raw.niggas_in_paris,android.R.drawable.ic_media_play));
+        songs.add(new Song("See you Again", "Miley Cyrus",R.raw.see_you_again,android.R.drawable.ic_media_play));
+        songs.add(new Song("Work it", "Missy Elliot",R.raw.missy_workit,android.R.drawable.ic_media_play));
+        songs.add(new Song("Bang Bang Bang", "Mark Ronson",R.raw.bang_bang_bang,android.R.drawable.ic_media_play));
+        songs.add(new Song("Unbreak my Heart", "Toni braxton",R.raw.unbreak_my_heart,android.R.drawable.ic_media_play));
         SongAdapter adapter = new SongAdapter(getActivity(),R.color.category_albums,songs);
         final ListView listView = (ListView) rootView.findViewById(R.id.list);
-        pauseButton = (ImageButton) rootView.findViewById(R.id.bt_start_pause);
-        playButton = (ImageButton) rootView.findViewById(R.id.bt_start_play);
-        fast_forward = (ImageButton) rootView.findViewById(R.id.fast_forward);
-        rewind = (ImageButton) rootView.findViewById(R.id.rewind);
+
+
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -121,49 +149,80 @@ public class SongsFragment extends Fragment {
                     sMediaPlayer = MediaPlayer.create(getActivity(), song.getAudioResourceId());
 
                     // Start the audio file
+
                     sMediaPlayer.start();
+                    playView.setVisibility(View.VISIBLE);
+                    playButton.setImageResource(android.R.drawable.ic_media_pause);
+                    selectedFile.setText(song.getArtistName() + " - "+ song.getSongTitle());
 
                     // Setup a listener on the media player, so that we can stop and release the
                     // media player once the sound has finished playing.
                     sMediaPlayer.setOnCompletionListener(sOnCompletionListener);
+                    sMediaPlayer.setOnErrorListener(onError);
                 }}
         });
-
-        pauseButton.setOnClickListener(new View.OnClickListener() {
-
+        View.OnClickListener onClickListener = new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {switch (v.getId()) {
+                case play: {
+                    if (sMediaPlayer.isPlaying()) {
+                        handler.removeCallbacks(updatePositionRunnable);
+                        sMediaPlayer.pause();
+                        playButton.setImageResource(android.R.drawable.ic_media_play);
+                    } else {
+                        if (isStarted) {
+                            sMediaPlayer.start();
+                            playButton.setImageResource(android.R.drawable.ic_media_pause);
 
+                            updatePosition();
+                        } else {
+                            startPlay(currentFile);
+                        }
+                    }
 
-                if (paused){
-                    sMediaPlayer.pause();
-                    playButton.setVisibility(View.VISIBLE);
+                    break;
                 }
+                case R.id.next: {
+                    int seekto = sMediaPlayer.getCurrentPosition() + STEP_VALUE;
 
+                    if (seekto > sMediaPlayer.getDuration())
+                        seekto = sMediaPlayer.getDuration();
 
+                    sMediaPlayer.pause();
+                    sMediaPlayer.seekTo(seekto);
+                    sMediaPlayer.start();
 
+                    break;
+                }
+                case R.id.prev: {
+                    int seekto = sMediaPlayer.getCurrentPosition() - STEP_VALUE;
+
+                    if (seekto < 0)
+                        seekto = 0;
+
+                    sMediaPlayer.pause();
+                    sMediaPlayer.seekTo(seekto);
+                    sMediaPlayer.start();
+
+                    break;
+                }
             }
-        });
+            }
 
-        playButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pauseButton.setVisibility(View.VISIBLE);
-                sMediaPlayer.start();
-            }
-        });
-        fast_forward.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sMediaPlayer.seekTo(rewind_sec+=3000);
 
-            }
-        });rewind.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sMediaPlayer.seekTo(rewind_sec-=3000);
-            }
-        });
+
+        };
+
+
+
+       playButton.setOnClickListener(onClickListener);
+        fast_forward.setOnClickListener(onClickListener);
+        rewind.setOnClickListener(onClickListener);
+
+
+
+
+
 
 
 
@@ -178,11 +237,21 @@ public class SongsFragment extends Fragment {
         return rootView;
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(updatePositionRunnable);
+        sMediaPlayer.stop();
+        sMediaPlayer.reset();
+        sMediaPlayer.release();
 
-
-    public SongsFragment() {
+        sMediaPlayer = null;
 
     }
+
+
+
+    public SongsFragment() {}
 
     private void releaseMediaPlayer() {
         // If the media player is not null, then it may be currently playing a sound.
@@ -201,5 +270,54 @@ public class SongsFragment extends Fragment {
             sAudioManager.abandonAudioFocus(sOnAudioFocusChangeListener);
         }
     }
+    private void updatePosition() {
+        handler.removeCallbacks(updatePositionRunnable);
+
+        handler.postDelayed(updatePositionRunnable, UPDATE_FREQUENCY);
+    }
+
+
+    private void stopPlay() {
+        sMediaPlayer.stop();
+        sMediaPlayer.reset();
+        playButton.setImageResource(android.R.drawable.ic_media_play);
+        handler.removeCallbacks(updatePositionRunnable);
+
+        isStarted = false;
+    }
+
+    private void startPlay(String file) {
+        Log.i("Selected: ", file);
+
+        selectedFile.setText(file);
+
+        sMediaPlayer.stop();
+        sMediaPlayer.reset();
+
+        try {
+            sMediaPlayer.setDataSource(file);
+            sMediaPlayer.prepare();
+            sMediaPlayer.start();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        playButton.setImageResource(android.R.drawable.ic_media_pause);
+
+        updatePosition();
+
+        isStarted = true;
+    }
+
+
+
+
+
+
+
 
 }
